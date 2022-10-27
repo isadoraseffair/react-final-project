138f656fb70c4e74efc4a369dc142562629e394b
Isadora Seffair <isadoraseffair@hotmail.com>
2022-10-27 11:43:52 +0100

    Added github url on footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      React Weather App
+      <h1>React Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/isadoraseffair/react-final-project"
+          target="blank"
+        >
+          Open-source code {" "}
+        </a>
+        by Isadora Seffair
+      </footer>
     </div>
   );
 }
-
-
