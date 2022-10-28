a06774a485d57cf1a94a90a3d277e50759122b21
Isadora Seffair <isadoraseffair@hotmail.com>
2022-10-28 17:01:00 +0100

    Added some CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>React Weather App</h1>
+      <h1 className="heading">React Weather App</h1>
+      <Weather />
       <footer>
         <a
           href="https://github.com/isadoraseffair/react-final-project"
